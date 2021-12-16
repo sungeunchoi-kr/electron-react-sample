d6c4e655f1a6888bfad20a386510ef42c0fefdef
Sung E. Choi <sungeunchoi.kr@gmail.com>
2021-12-16 17:43:31 +0900

    Minimal working sample

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    electron-react-sample
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
